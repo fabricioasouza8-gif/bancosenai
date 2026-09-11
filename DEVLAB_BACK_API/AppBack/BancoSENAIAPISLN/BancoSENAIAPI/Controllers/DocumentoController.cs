fce4c5ef10e3dc239a2a182873eb0466a1354e73
fabricioasouza8-gif <fabricio.a.souza8@aluno.senai.br>
2026-09-11 08:37:39 -0300

    feat(documento): simulacao do banco de dados

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -4,6 +4,12 @@ namespace BancoSENAIAPI.Controllers
 {
     public class DocumentoController : Controller
     {
+        private readonly string _caminhoRaiz = Path.Combine(
+            Directory.GetCurrentDirectory(), "ClienteArquivos"
+            );
 
+        private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado>();
+
+        private static int _nextId = 1;
     }
 }
