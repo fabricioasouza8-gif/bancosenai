aa3fdc56e667800fe0120b84413d5374aafc2aaa
fabricioasouza8-gif <fabricio.a.souza8@aluno.senai.br>
2026-09-11 09:08:18 -0300

    feat(documentos): definicao metadados no upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -26,6 +26,14 @@ namespace BancoSENAIAPI.Controllers
             {
                 Directory.CreateDirectory(pastaCliente);
             }
+
+            string extensao = Path.GetExtension(arquivo.FileName);
+
+            string nameOriginal = Path.GetFileNameWithoutExtension(arquivo.FileName);
+
+            string novoNome = $"{codigoCliente}_{nameOriginal}_{Guid.NewGuid()}{extensao}";
+
+            string caminhoFinal = Path.Combine(pastaCliente, novoNome);
         }
     }
 }
